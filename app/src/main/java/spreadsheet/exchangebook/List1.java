179374b037d2fb8133bb9efb424fa1b866b54abc
Mitrand <mitrandil.illusion@gmail.com>
2016-11-11 18:15:30 +0200

    form 2

---
+++ app/src/main/java/spreadsheet/exchangebook/List1.java
@@ -0,0 +1,13 @@
+package spreadsheet.exchangebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class List1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list1);
+    }
+}
